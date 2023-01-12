b92646c513dddca1f6af3a9bd77de921faf752e9
Corey Hastings <stingzld@gmail.com>
2023-01-12 10:15:36 -0600

    Added letter template and birthdays CSV. Completed main code.

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+import pandas
 import smtplib
+import datetime as dt
 
-EMAIL_SENDER = "stingzld.test.email@gmail.com"
-EMAIL_RECIPIENT = "stingzld.other.test.email@gmail.com"
-PASSWORD = "aaeddmspdtbyixxw"
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=EMAIL_SENDER, password=PASSWORD)
-    connection.sendmail(from_addr=EMAIL_SENDER,
-                        to_addrs=EMAIL_RECIPIENT,
-                        msg="Subject:Hello World!\n\n"
-                            "This is the body of the email!")
+def send_telegram(email, message):
+    email_sender = "stingzld.test.email@gmail.com"
+    password = "aaeddmspdtbyixxw"
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=email_sender, password=password)
+        connection.sendmail(from_addr=email_sender,
+                            to_addrs=email,
+                            msg="Subject:Happy Birthday!\n\n"
+                                f"{message}")
+
+
+def create_letter(name):
+    with open("letter.txt") as file:
+        contents = file.read()
+    return contents.replace("[NAME]", name)
+
+
+today = (dt.datetime.now().month, dt.datetime.now().day)
+data = pandas.read_csv("birthdays.csv")
+
+for (index, row) in data.iterrows():
+    birthday = (row.month, row.day)
+    if birthday == today:
+        letter = create_letter(row["name"])
+        send_telegram(row.email, letter)
