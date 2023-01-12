79d98ace2dfef90e6fc1db8a353a0f239768f960
Corey Hastings <stingzld@gmail.com>
2023-01-12 09:42:12 -0600

    Created initial SMTP code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Automated Birthday Wisher
+
+This is a simple program that will automatically send out emails to wish people a 
+Happy Birthday!
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import smtplib
 
+EMAIL_SENDER = "stingzld.test.email@gmail.com"
+EMAIL_RECIPIENT = "stingzld.other.test.email@gmail.com"
+PASSWORD = "aaeddmspdtbyixxw"
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=EMAIL_SENDER, password=PASSWORD)
+    connection.sendmail(from_addr=EMAIL_SENDER,
+                        to_addrs=EMAIL_RECIPIENT,
+                        msg="Subject:Hello World!\n\n"
+                            "This is the body of the email!")
